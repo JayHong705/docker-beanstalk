a31d360c1dbdc8a629cd2f3cbe7b524f9550bbf9
Admin <contact@hellowoot.co.kr>
2021-07-15 23:41:20 +0900

    add profile url

--- research/config/urls.py
+++ research/config/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from poll import views
+from account import views
+
 
 router = DefaultRouter()
 router.register(r'poll', views.PollViewSet)
+router.register(r'profile', views.ProfileViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
