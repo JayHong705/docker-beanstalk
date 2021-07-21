8b68e4267906683e44039417a9a34d80b9638fa0
Admin <contact@hellowoot.co.kr>
2021-07-21 18:55:23 +0900

    add jwt token authentication settings

--- research/config/settings/base.py
+++ research/config/settings/base.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 RUN_MODE = os.getenv('RUN_MODE')
 
@@ -94,6 +95,25 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
+JWT_AUTH = {
+    'JWT_SECRET_KEY': SECRET_KEY,
+    'JWT_ALGORITHM': 'HS256',
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_EXPIRATION_DELTA': timedelta(days=60),
+    'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=365),
+}
 
 
 # Internationalization
